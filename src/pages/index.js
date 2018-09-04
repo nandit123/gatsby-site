a68fd9c9b7be6334e16899b3dd5d48141620dda4
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2018-09-04 23:48:10 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hey There, I am Nandit Mehra. I love to code and learn new technologies to build great stuff. I learned many stuff from freeCodeCamp</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
